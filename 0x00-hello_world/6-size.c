7525c7659f96e489733127a0f60387bb171d87f7
Aellun <okello.k.otieno@gmail.com>
2023-06-15 16:42:42 -0700

    change the entir line of code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,15 @@
 #include<stdio.h>
 /**
-* main - sizeof type
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char n1;
-
-	int n2;
-
-	long  int n3;
-
-	long long n4;
-
-	float n5;
-
-	printf("Size of a char:%d byte(s)\n", (unsigned long)sizeof(n1));
-	printf("Size of a int:%d byte(s)\n", (unsigned long)sizeof(n2));
-	printf("Size of a long int:%d byte(s)\n", (unsigned long)sizeof(n3));
-	printf("Size of a long long int:%d byte(s)\n", (unsigned long)sizeof(n4));
-	printf("Size of a float:%d byte(s)\n", (unsigned long)sizeof(n5));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
