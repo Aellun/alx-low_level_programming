078b6d50fd97fb4ea10caa6cbeb7a4507839c316
Aellun <okello.k.otieno@gmail.com>
2023-06-15 16:37:26 -0700

    changed format specifier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 
 	float n5;
 
-	printf("Size of a char:%c byte(s)\n", (unsigned long)sizeof(n1));
+	printf("Size of a char:%d byte(s)\n", (unsigned long)sizeof(n1));
 	printf("Size of a int:%d byte(s)\n", (unsigned long)sizeof(n2));
-	printf("Size of a long int:%ld byte(s)\n", (unsigned long)sizeof(n3));
-	printf("Size of a long long int:%li byte(s)\n", (unsigned long)sizeof(n4));
-	printf("Size of a float:%f byte(s)\n", (unsigned long)sizeof(n5));
+	printf("Size of a long int:%d byte(s)\n", (unsigned long)sizeof(n3));
+	printf("Size of a long long int:%d byte(s)\n", (unsigned long)sizeof(n4));
+	printf("Size of a float:%d byte(s)\n", (unsigned long)sizeof(n5));
 	return (0);
 }
