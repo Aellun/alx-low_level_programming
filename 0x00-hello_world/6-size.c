a610db587dc34297a551220ee722c42ac2bfc870
Aellun <okello.k.otieno@gmail.com>
2023-06-15 16:17:29 -0700

    upeercase to tje leteer s and changed the variable placement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/**
- * main - this program print the various sizeof 
+/** main - this program print the various sizeof 
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -17,8 +16,8 @@ int main(void)
 
 	printf("Size of a char:%c byte(s)\n", (unsigned long)sizeof(n1));
 	printf("Size of a int:%d byte(s)\n", (unsigned long)sizeof(n2));
-	printf("Size of a long:%ld byte(s)\n", (unsigned long)sizeof(n3));
-	printf("Size of a long long:%li byte(s)\n", (unsigned long)sizeof(n4));
+	printf("Size of a long int:%ld byte(s)\n", (unsigned long)sizeof(n3));
+	printf("Size of a long long int:%li byte(s)\n", (unsigned long)sizeof(n4));
 	printf("Size of a float:%f byte(s)\n", (unsigned long)sizeof(n5));
 	return (0);
 }
