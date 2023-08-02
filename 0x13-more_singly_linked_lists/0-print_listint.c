16a2985b6b8c01afff0022eb74b1509df70d514e
Aellun <okello.k.otieno@gmail.com>
2023-08-02 08:31:51 -0700

    corrected white space

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,16 @@
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h) 
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL) 
+	while (h != NULL)
 	{
 		printf("%d\n", h->data);
 		h = h->next;
 		count++;
 	}
 
-	return count;
+	return (count);
 }
