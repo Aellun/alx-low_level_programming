1cc66304fbb5c792e302f2a7c94c6fa2b346b32a
Aellun <okello.k.otieono@gmail.com>
2023-10-09 12:53:18 -0700

    fixed free memory error

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,28 +10,25 @@
  * for keys, values, nodes, and the array itself. After deletion, the hash
  * table ptr itself becomes invalid and should not be used further.
  */
-void hash_table_delete(hash_table_t *ht)
-{
-	if (ht == NULL)
+
+
+void hash_table_delete(hash_table_t *ht) {
+	if (ht == NULL) {
 		return;
+	}
 
-	unsigned long int e;
-
-	for (e = 0; e < ht->size; e++)
-	{
-		hash_node_t *node = ht->array[e];
-
-		while (node != NULL)
-		{
+	/*Iterate through the hash table's array and linked lists, 
+	  freeing memory allocated for keys, values, nodes, and the array itself.*/
+	for (unsigned long int c = 0; c < ht->size; c++) {
+		hash_node_t *node = ht->array[c];
+		while (node != NULL) {
 			hash_node_t *tmp = node;
-
 			node = node->next;
 			free(tmp->key);
 			free(tmp->value);
 			free(tmp);
 		}
 	}
-
 	free(ht->array);
 	free(ht);
 }
