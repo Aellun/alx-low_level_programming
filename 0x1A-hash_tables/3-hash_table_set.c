b299157a8b1f90e2f6ef0badae958ae0a7be2322
Aellun <okello.k.otieono@gmail.com>
2023-10-09 03:44:31 -0700

    fixed issue with error handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,12 @@
 #include "hash_tables.h"
-
+#include <string.h>
+#include <stdlib.h>
 /**
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A ptr to the hash table.
  * @key: The key to update != empty str
  * @value: The value correlated to the key.
- *
- * Return: 1 on success 0 on otherwise.
- *
+ * Return: 1 on success 0 on otherwise
  * Description:
  * This function adds or updates an element in a hash table. It takes a pointer
  * to the hash table, a key, and a value as input. The key must not be an empty
@@ -23,12 +22,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+	{
+		free(value_copy);
+		return (0);/*Invalid index*/
+	}
 	for (j = index; ht->array[j]; j++)
 	{
 		if (strcmp(ht->array[j]->key, key) == 0)
@@ -38,22 +40,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
 		free(value_copy);
-		return (0);
+		return (0);/*Memory allocation failed*/
 	}
 	new->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
-		return (0);
+		free(value_copy);
+		return (0);/*Memory allocation failed*/
 	}
 	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
-
 	return (1);
 }
