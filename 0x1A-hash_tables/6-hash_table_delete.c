816349a148a26792cd81c28ee930a40fa00aea78
Aellun <okello.k.otieono@gmail.com>
2023-10-11 17:31:06 -0700

    fixed the for loop

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,25 +10,27 @@
  * for keys, values, nodes, and the array itself. After deletion, the hash
  * table ptr itself becomes invalid and should not be used further.
  */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int item;
 
-
-void hash_table_delete(hash_table_t *ht) {
-	if (ht == NULL) {
-		return;
-	}
-
-	/*Iterate through the hash table's array and linked lists, 
-	  freeing memory allocated for keys, values, nodes, and the array itself.*/
-	for (unsigned long int c = 0; c < ht->size; c++) {
-		hash_node_t *node = ht->array[c];
-		while (node != NULL) {
-			hash_node_t *tmp = node;
-			node = node->next;
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
+	for (item = 0; item < ht->size; item++)
+	{
+		if (ht->array[item] != NULL)
+		{
+			node = ht->array[item];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
