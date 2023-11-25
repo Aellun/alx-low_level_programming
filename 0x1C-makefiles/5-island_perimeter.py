870974c8ae1f370c42e96f339b8fc6fb1b9b5123
Aellun <okello.k.otieono@gmail.com>
2023-11-25 01:15:46 -0800

    created 5-main.py and the tests directory

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,15 +14,11 @@ def island_perimeter(grid):
 
     if not grid or not grid[0]:
         return perimeter
-
     rows, cols = len(grid), len(grid[0])
-
     for w in range(rows):
         for h in range(cols):
             if grid[w][h] == 1:
                 perimeter += 4
-
-
                 if w > 0 and grid[w - 1][h] == 1:
                     perimeter -= 2
                 if h > 0 and grid[w][h - 1] == 1:
