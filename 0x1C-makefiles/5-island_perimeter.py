2ab5d943edf2fd0aef164292f0bb7e5a79a4a6a1
Aellun <okello.k.otieono@gmail.com>
2023-11-24 21:47:10 -0800

    initial commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculates the island perimeter
+
+    Args:
+    - grid (list of list of integers): Represents the island grid.
+
+    Returns:
+    - int: The island perimeter
+    """
+    perimeter = 0
+
+    if not grid or not grid[0]:
+        return perimeter
+
+    rows, cols = len(grid), len(grid[0])
+
+    for w in range(rows):
+        for h in range(cols):
+            if grid[w][h] == 1:
+                perimeter += 4
+
+
+                if w > 0 and grid[w - 1][h] == 1:
+                    perimeter -= 2
+                if h > 0 and grid[w][h - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,5 @@
+0x1C. C - Makefiles
+In this project am working on makefile for the files below:
+school.c
+main.c
+m.h
